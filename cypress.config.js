591826fa1b22c3c26075a6d89947782b98054902
Soumya Mishra <soumya_rm@outlook.com>
2023-01-16 18:33:53 +0530

    added mochaawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -8,9 +8,11 @@ module.exports = defineConfig({
   viewportHeight: 1000,
   chromeWebSecurity: true,
   filterSpecs: true,
+  reporter: 'cypress-mochawesome-reporter', //reporter Key
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here'
+      require('cypress-mochawesome-reporter/plugin')(on) // reporter listner
     },
     specPattern: [
       'cypress/e2e/apiTests/getUsers.spec.cy.js',
